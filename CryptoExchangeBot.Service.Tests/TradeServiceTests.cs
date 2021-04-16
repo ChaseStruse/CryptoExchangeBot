be3e0842fd221aeb03d6a4a375fb663ac0cd4d9c
Chase <chasestruse@gmail.com>
2021-04-15 21:59:18 -0500

    Added failing test for buy call

---
+++ CryptoExchangeBot.Service.Tests/TradeServiceTests.cs
@@ -0,0 +1,35 @@
+using FluentAssertions;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace CryptoExchangeBot.Service.Tests
+{
+    public class TradeServiceTests
+    {
+        private TradeService _sut;
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new TradeService();
+        }
+
+        [Test]
+        public void BuyReturnsTrue_WhenGiven_CryptosLastThreeMonthPrices()
+        {
+            var lastThreeMonthHighs = new List<double>()
+            {
+                .5,
+                .6,
+                .7,
+                .8,
+                1.2
+            };
+
+            var currentPrice = 1.35;
+
+            var actual = _sut.Buy(lastThreeMonthHighs, currentPrice);
+
+            actual.Should().Be(true);
+        }
+    }
+}
