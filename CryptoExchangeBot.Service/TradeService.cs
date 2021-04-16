8f5f7f1ff8bc07ac986a3d91d74a834db086e9ae
Chase <chasestruse@gmail.com>
2021-04-15 21:59:30 -0500

    Added method stub

---
+++ CryptoExchangeBot.Service/TradeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CryptoExchangeBot.Service
+{
+    public interface ITradeService
+    {
+        bool Buy(List<double> lastThreeMonthHighs, double currentPrice);
+    }
+    public class TradeService
+    {
+        public bool Buy(List<double> lastThreeMonthHighs, double currentPrice)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
